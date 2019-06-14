c230ea228f713ef57a52654504adc11435e9de4d
gitClient <17310929526@163.com>
2019-06-14 16:55:43 +0800

    2

---
+++ src/main/java/xxx/xxx/testController/JenkinsController.java
@@ -0,0 +1,13 @@
+package xxx.xxx.testController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+    @RequestMapping("/jenkins")
+    public String jenkins(){
+        System.out.println("测试完成");
+        return "测试完成";
+    }
+}
