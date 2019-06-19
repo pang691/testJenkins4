1ae0023c1e119ec98b1c56ce149fded59e039c6c
gitClient <17310929526@163.com>
2019-06-19 13:42:03 +0800

    1

--- src/main/java/xxx/xxx/testController/JenkinsController.java
+++ src/main/java/xxx/xxx/testController/JenkinsController.java
@@ -12,7 +12,9 @@ public class JenkinsController {
     }
     @RequestMapping("/jenkinsQuartz")
     public String jenkinsQuartz(){
-        System.out.println("测试完成--jenkins定时任务自动部署完成qqqq");
-        return "测试完成--jenkins定时任务自动部署完成qqqqq";
+        System.out.println("测试完成--jenkins定时任务自动部署完成qq休息休息");
+        return "测试完成--jenkins定时任务自动部署完成qqq休息休息";
     }
+
+
 }
