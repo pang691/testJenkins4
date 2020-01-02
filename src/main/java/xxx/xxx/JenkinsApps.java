c222bbe6e3c92688a4bda4374b6af24eb3a87045
gitClient <17310929526@163.com>
2020-01-02 13:51:37 +0800

    first

--- src/main/java/xxx/xxx/JenkinsApps.java
+++ src/main/java/xxx/xxx/JenkinsApps.java
@@ -1,11 +1,11 @@
-//package xxx.xxx;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class JenkinsApps {
-//    public static void main(String[] args) {
-//        SpringApplication.run(JenkinsApps.class,args);
-//    }
-//}
+package xxx.xxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApps {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApps.class,args);
+    }
+}
