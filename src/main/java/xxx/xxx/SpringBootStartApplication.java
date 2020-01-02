87db723d6242d1854fbf03ba381da53f9c11ba1b
gitClient <17310929526@163.com>
2020-01-02 13:48:20 +0800

    first

--- src/main/java/xxx/xxx/SpringBootStartApplication.java
+++ src/main/java/xxx/xxx/SpringBootStartApplication.java
@@ -1,16 +1,23 @@
 package xxx.xxx;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Component;
 
 /**
  * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
  */
+@SpringBootApplication
 public class SpringBootStartApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
-        return builder.sources(JenkinsApps.class);
+        return builder.sources(SpringBootStartApplication.class);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStartApplication.class,args);
     }
 }
